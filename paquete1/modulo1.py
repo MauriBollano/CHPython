b1c31d0333bfe929b15a2c2bc54c1e2f0758d216
Mauricio Bollano <bollanomauricio@gmail.com>
2022-12-19 19:32:31 -0300

    Modificacion de primer entrega

--- paquete1/modulo1.py
+++ paquete1/modulo1.py
@@ -3,18 +3,29 @@ from google.colab import drive
 drive.mount('/drive/')
 
 ruta= '/drive/MyDrive/Primer pre-entrega+Bollano """
-
+import json
 """ Funciones """
-def registro():
-    Usuario = str(input("Ingrese un usuario:  "))
-    Contraseña = str(input("Ingrese una contraseña:  "))
-    n={"USUARIO": Usuario, "CONTRASEÑA": Contraseña}
-    database= open( ruta + '/BaseDatos.txt','a')
-    database.write("Usuario: "+n["USUARIO"] +" "+"Contraseña:  " +n["CONTRASEÑA"]+'\n')
-    database.close()
+def registro(database):
+    user = input("Ingrese un usuario:  ")
+    password = input("Ingrese una contraseña:  ")
 
-def leerdb():
-    database= open( ruta + '/BaseDatos.txt','r')
-    cont=database.read()
-    print(cont)
-    database.close()
+    database.update({user:password})
+
+def leerdb(database):
+    print("La database contiene :")
+    for key,value in database.items():
+        print(key,":",value)
+
+def saveArchive(database):
+    ruta= r"C:\Users\Usuario\Desktop\Python\CHPython\paquete1"
+    with open(ruta+r"\database.txt","w") as file:
+        json.dump(database,file,indent=4)
+
+def login(database):  
+    user = input("Ingrese un usuario:  ")
+    if user in database.keys():
+        password = input("Ingrese su contraseña:  ")
+    if database[user] == password :
+        return "Sesion iniciada"
+    else:
+        return "Contraseña incorrecta"
