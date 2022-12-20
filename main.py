5be9bb7c9347083c8bfa97134bf5b0c32a2f27a3
Mauricio Bollano <bollanomauricio@gmail.com>
2022-12-20 19:56:49 -0300

    a

--- main.py
+++ main.py
@@ -31,12 +31,12 @@ Los metodos de cliente son:
 """
 cliente1 = Cliente("mauricio", 45678, "mb@gmail.com", 1000, ["moda","computacion"]) 
 
-print(cliente1)
+""" print(cliente1)
 
 print("el saldo es: ",cliente1.saldo)
 
-cliente1.cargar_saldo(100)
+cliente1.cargar_saldo(500) 
+"""
+""" print("el saldo es: ",cliente1.saldo)
 
-print("el saldo es: ",cliente1.saldo)
-
-cliente1.comprar("computadora","800")
+cliente1.comprar("computadora","800") """
