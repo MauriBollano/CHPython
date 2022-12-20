16b14e8b1b41ee248e1e924c9306be607689f6f1
Mauricio Bollano <bollanomauricio@gmail.com>
2022-12-20 19:23:00 -0300

    Agregado de comentarios

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
-""" from entrega2 import Cliente """
+from entrega2 import Cliente
 from entrega1 import Inicio
 
+""" 
+Esto es para entrega 1: Modifique el codigo para realizar el registro, login y lectura de la base de datos con diccionarios y objetos pero aun no anda
+
+"""  
+"""
 def main():
     us = Inicio()
     us.menu()
 
 if __name__ == '__main__':
     main()
+"""  
 
 
-""" cliente1 = Cliente("mauricio", 45678, "mb@gmail.com", 1000, ["moda","computacion"]) """
+""" 
+Esto es para entrega 2: Crea un objeto cliente con los atributos: 
+- Nombre
+- N Telefono
+- Email
+- Saldo a favor
+- Gustos/Intereses
 
+Los metodos de cliente son:
 
-""" print(cliente1)  """
+- cargar_saldo(Carga saldo a la cuenta del cliente)
+- comprar ( verifica el saldo dispoible e imprime la compra, el costo y el saldo restante si este es suficiente)
 
-""" print("el saldo es: ",cliente1.saldo) """
+"""
+cliente1 = Cliente("mauricio", 45678, "mb@gmail.com", 1000, ["moda","computacion"]) 
 
-""" cliente1.cargar_saldo(100) """
+print(cliente1)
 
-""" print("el saldo es: ",cliente1.saldo) """
+print("el saldo es: ",cliente1.saldo)
 
-""" cliente1.comprar("computadora","800") """
+cliente1.cargar_saldo(100)
 
-""" registro("database") """
+print("el saldo es: ",cliente1.saldo)
 
-""" leerdb(database) """
+cliente1.comprar("computadora","800")
